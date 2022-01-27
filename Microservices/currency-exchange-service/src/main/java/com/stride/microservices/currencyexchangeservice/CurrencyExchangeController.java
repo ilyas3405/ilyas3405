b97954e5318e5067c6ee2ce5878ac53dfa0c18bb
ilyas3405 <55305974+ilyas3405@users.noreply.github.com>
2022-01-27 10:02:42 -0600

    Add files via upload

---
+++ Microservices/currency-exchange-service/src/main/java/com/stride/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,39 @@
+package com.stride.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@Autowired
+	private CurrencyExchangeRepository repository;
+	
+	@Autowired
+	private Environment environment;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to)
+	{
+		
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		
+		if(currencyExchange == null) {
+			throw new RuntimeException("Unable to Find data for " +from +" and " +to );
+		}
+		
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		System.out.println(currencyExchange.getEnvironment());
+		return currencyExchange;
+		
+		
+	}
+}
